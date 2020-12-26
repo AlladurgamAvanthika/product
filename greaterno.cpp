97445a124a2f739c938124319063b284fd9b5a86
AlladurgamAvanthika <75297587+AlladurgamAvanthika@users.noreply.github.com>
2020-12-26 11:27:24 +0530

    Add files via upload

---
+++ greaterno.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int num;
+	cout<<"enter the num";
+	cin>>num;
+	if(num<50)
+	{
+cout<<"num is less than 50";
+	}
+else{
+	cout<<"num is greater than or equal to 50";
+	
+}return 0;
+}
